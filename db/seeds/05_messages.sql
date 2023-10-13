113bd79e58e47b772bb0644c0c06c50354f2df9d
dialop <dianalop1998@gmail.com>
2023-10-12 22:19:23 -0400

    Delete unnecessary files

--- db/seeds/05_messages.sql
+++
@@ -1,7 +0,0 @@
---------------------- MESSAGES TABLE SEEDS ---------------------
-
-INSERT INTO messages
-(sender_id, receiver_id, product_id, message, date_sent) 
-VALUES 
-(1, 2, 2, 'Is the Retro Blue Sneakers still available?', CURRENT_TIMESTAMP),
-(2, 1, 1, 'What is the fit for the Retro Red Dunk?', CURRENT_TIMESTAMP);
