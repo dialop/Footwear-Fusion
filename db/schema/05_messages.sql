9cc253117674e779f60df2561e83eac6d5196f59
dialop <dianalop1998@gmail.com>
2023-10-12 20:32:03 -0400

    Add messages table

---
+++ db/schema/05_messages.sql
@@ -0,0 +1,11 @@
+-- DROP and CREATE messages TABLE
+
+DROP TABLE IF EXISTS messages CASCADE;
+CREATE TABLE messages (
+    id SERIAL PRIMARY KEY,
+    sender_id INT REFERENCES users(id),
+    receiver_id INT REFERENCES users(id),
+    product_id INT REFERENCES products(id),
+    message TEXT NOT NULL,
+    date_sent TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
