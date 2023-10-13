1b1a70b401c1718bc4374789e7074f3b402ca45b
dialop <dianalop1998@gmail.com>
2023-10-12 22:12:31 -0400

    Resolved merge conflicts

---
+++ db/seeds/02_products.sql
@@ -0,0 +1,6 @@
+--------------------- PRODUCTS TABLE SEEDS ---------------------
+
+INSERT INTO products (title, model, description, size, color, price, owner_id, photo_url) 
+VALUES 
+('Picante Red', 'RS101', 'Retro Low Dunk Shoes', '9', 'Red', 76.96, 1, 'https://ca.kith.com/cdn/shop/files/AURORA_DV0831-103_PHSUH000-2000_1080x.jpg?v=1695305835'),
+('Sky Blue', 'BS202', 'Retro Low Dunk Shoes', '10', 'Blue', 79.99, 2, 'https://ca.kith.com/cdn/shop/files/AURORA_DV0831-104_PHSRH000-2000_1080x.jpg?v=1694628574');
