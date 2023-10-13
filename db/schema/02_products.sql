d3c76a8f8e7cda81585e79de57b92d3e803efbec
dialop <dianalop1998@gmail.com>
2023-10-12 20:10:06 -0400

    Add products table

---
+++ db/schema/02_products.sql
@@ -0,0 +1,15 @@
+-- DROP and CREATE products TABLE
+
+DROP TABLE IF EXISTS products CASCADE;
+CREATE TABLE products (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    model VARCHAR(255),
+    description TEXT,
+    size VARCHAR(50),
+    color VARCHAR(50),
+    price DECIMAL(10,2),
+    owner_id INT REFERENCES users(id),
+    is_sold BOOLEAN DEFAULT FALSE,
+    photo_url TEXT
+);
