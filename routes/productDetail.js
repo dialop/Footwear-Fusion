afefe4459f3084e76955873554cf38736f989d2a
Diana Ichmoukhametov <akhtyamovadiana.gmail.com>
2023-10-14 10:40:14 -0400

    Add product detail template and route

---
+++ routes/productDetail.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router  = express.Router();
+
+// This route doesn't work yet.
+//Diana Ichmoukhametov
+
+
+// router.get('/products/:id', (req, res) => {
+//   console.log('it is details');
+//   res.render('productDetail')
+// });
+
+
+module.exports = router;
