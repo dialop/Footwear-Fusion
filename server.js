8310210a6cd20aabb8453ba4afe35547b3312a2a
dialop <dianalop1998@gmail.com>
2023-10-14 23:37:07 -0400

    Add favorite router

--- server.js
+++ server.js
@@ -26,6 +26,10 @@ app.use(
 );
 app.use(express.static('public'));
 
+// Diana Lopez- FAVORITES ROUTER
+const favoritesRouter = require('./routes/favorites'); 
+
+
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
 const userApiRoutes = require('./routes/users-api');
@@ -42,6 +46,8 @@ app.use('/api/widgets', widgetApiRoutes);
 app.use('/users', usersRoutes);
 app.use('/products', productsRoutes);
 app.use('/productDetail', productDetails)
+app.use('/favorites', favoritesRouter);
+
 
 // Note: mount other resources here, using the same pattern above
 
