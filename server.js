c95c1424e0c242958e249cf40026d8fd9005a523
Diana Ichmoukhametov <akhtyamovadiana.gmail.com>
2023-10-14 22:40:39 -0400

    Fixed errors

--- server.js
+++ server.js
@@ -1,4 +1,63 @@
 
+// // load .env data into process.env
+// require('dotenv').config();
+
+// // Web server config
+// const sassMiddleware = require('./lib/sass-middleware');
+// const express = require('express');
+// const morgan = require('morgan');
+// const bcrypt = require('bcrypt');
+// const cookieSession = require('cookie-session');
+
+// const PORT = process.env.PORT || 8080;
+// const app = express();
+
+// app.set('view engine', 'ejs');
+
+// // Load the logger first so all (static) HTTP requests are logged to STDOUT
+// // 'dev' = Concise output colored by response status for development use.
+// //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
+// app.use(morgan('dev'));
+// app.use(express.urlencoded({ extended: true }));
+// app.use(
+//   '/styles',
+//   sassMiddleware({
+//     source: __dirname + '/styles',
+//     destination: __dirname + '/public/styles',
+//     isSass: false, // false => scss, true => sass
+//   })
+// );
+// app.use(express.static('public'));
+
+// // Separated Routes for each Resource
+// // Note: Feel free to replace the example routes below with your own
+// const userApiRoutes = require('./routes/users-api');
+// const widgetApiRoutes = require('./routes/widgets-api');
+// const usersRoutes = require('./routes/users');
+
+// // Mount all resource routes
+// // Note: Feel free to replace the example routes below with your own
+// // Note: Endpoints that return data (eg. JSON) usually start with `/api`
+// app.use('/api/users', userApiRoutes);
+// app.use('/api/widgets', widgetApiRoutes);
+// app.use('/users', usersRoutes);
+// // Note: mount other resources here, using the same pattern above
+
+// // Home page
+// // Warning: avoid creating more routes in this file!
+// // Separate them into separate routes files (see above).
+
+// app.get('/', (req, res) => {
+//   res.render('index');
+// });
+
+// app.listen(PORT, () => {
+//   console.log(`Example app listening on port ${PORT}`);
+// });
+
+
+
+
 // load .env data into process.env
 require('dotenv').config();
 
@@ -6,8 +65,6 @@ require('dotenv').config();
 const sassMiddleware = require('./lib/sass-middleware');
 const express = require('express');
 const morgan = require('morgan');
-const bcrypt = require('bcrypt');
-const cookieSession = require('cookie-session');
 
 const PORT = process.env.PORT || 8080;
 const app = express();
@@ -34,6 +91,8 @@ app.use(express.static('public'));
 const userApiRoutes = require('./routes/users-api');
 const widgetApiRoutes = require('./routes/widgets-api');
 const usersRoutes = require('./routes/users');
+const productsRoutes = require('./routes/products')
+const productDetails = require('./routes/productDetail')
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
@@ -41,6 +100,9 @@ const usersRoutes = require('./routes/users');
 app.use('/api/users', userApiRoutes);
 app.use('/api/widgets', widgetApiRoutes);
 app.use('/users', usersRoutes);
+app.use('/products', productsRoutes);
+app.use('/productDetail', productDetails)
+
 // Note: mount other resources here, using the same pattern above
 
 // Home page
@@ -51,8 +113,16 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
+});
+
+// Add route product detail. Will move to routes later.
+// Diana Ichmoukhametov
+app.get('/products/:id', (req, res) => {
+  console.log('it is details');
+  res.render('productDetail')
 });
 
 
@@ -123,60 +193,3 @@ const getUserByEmail = function(email, database) {
   }
   return false;
 };
-
-
-// load .env data into process.env
-require('dotenv').config();
-
-// Web server config
-const sassMiddleware = require('./lib/sass-middleware');
-const express = require('express');
-const morgan = require('morgan');
-
-const PORT = process.env.PORT || 8080;
-const app = express();
-
-app.set('view engine', 'ejs');
-
-// Load the logger first so all (static) HTTP requests are logged to STDOUT
-// 'dev' = Concise output colored by response status for development use.
-//         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
-app.use(morgan('dev'));
-app.use(express.urlencoded({ extended: true }));
-app.use(
-  '/styles',
-  sassMiddleware({
-    source: __dirname + '/styles',
-    destination: __dirname + '/public/styles',
-    isSass: false, // false => scss, true => sass
-  })
-);
-app.use(express.static('public'));
-
-// Separated Routes for each Resource
-// Note: Feel free to replace the example routes below with your own
-const userApiRoutes = require('./routes/users-api');
-const widgetApiRoutes = require('./routes/widgets-api');
-const usersRoutes = require('./routes/users');
-const productsRoutes = require('./routes/products')
-
-// Mount all resource routes
-// Note: Feel free to replace the example routes below with your own
-// Note: Endpoints that return data (eg. JSON) usually start with `/api`
-app.use('/api/users', userApiRoutes);
-app.use('/api/widgets', widgetApiRoutes);
-app.use('/users', usersRoutes);
-app.use('/products', productsRoutes)
-// Note: mount other resources here, using the same pattern above
-
-// Home page
-// Warning: avoid creating more routes in this file!
-// Separate them into separate routes files (see above).
-
-app.get('/', (req, res) => {
-  res.render('index');
-});
-
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`);
-});
