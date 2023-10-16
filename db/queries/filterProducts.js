30ad969602b38633c5eeb274abc98e606643f983
dialop <dianalop1998@gmail.com>
2023-10-16 19:27:09 -0400

    Add ILIKE to provide case-sensitivity

--- db/queries/filterProducts.js
+++ db/queries/filterProducts.js
@@ -1,33 +1,57 @@
-// ---- HANDLES QUERY TO FILTER PRODUCTS ---- //
-
+// Database connection setup
 const db = require('../connection');
 
-/**
- * @param {Object} criteria - Object containing filtering criteria
- * @returns {Promise<Array>} List of filtered products
- * @throws {Error} If error filtering the products
- */
+// ---- HANDLES QUERY TO FILTER PRODUCTS ---- //
 
-const filterProducts = async(criteria) => {
-  let query = 'SELECT * FROM products WHERE ';
-  let values = [];
-  let conditions = [];
-
-  for (let key in criteria) {
-    if (Object.hasOwnProperty.call(criteria, key)) {
-      conditions.push(`${key} = $${values.length + 1}`);
-      values.push(criteria[key]);
-    }
+const getFilteredProducts = (products, limit = 10) => {
+  const queryParams = [];
+  
+  // Base query string to fetch products
+  let queryString = `
+  SELECT products.*
+  -- , AVG(product_reviews.rating) as average_rating
+  FROM products
+  LEFT JOIN product_reviews ON products.id = product_id
+  WHERE 1 = 1
+  `;
+  
+  // Add filter for product title
+  if (products.title) {
+    queryParams.push(`%${products.title}%`);
+    queryString += ` AND title ILIKE $${queryParams.length}`; // ILIKE provides case-sensitivity matching when filtering
   }
 
-  query += conditions.join(' AND ');
-
-  try {
-    const result = await db.query(query, values);
-    return result.rows;
-  } catch (error) {
-    throw new Error(`Failed to filter products based on criteria ${JSON.stringify(criteria)}: ${error.message}`);
+  // Add filter for product price range
+  if (products.minimum_price && products.maximum_price) {
+    queryParams.push(products.minimum_price, products.maximum_price);
+    queryString += ` AND price BETWEEN $${queryParams.length - 1} AND $${queryParams.length}`;
   }
+
+  // Group the results by product ID
+  queryString += `
+    GROUP BY products.id
+  `;
+
+  // Uncomment to filter by minimum rating
+  // if (products.minimum_rating) {
+  //   queryParams.push(products.minimum_rating);
+  //   queryString += ` HAVING AVG(product_reviews.rating) >= $${queryParams.length}`;
+  // }
+
+  // Add order and limit to the query
+  queryParams.push(limit);
+  queryString += `
+    ORDER BY price
+    LIMIT $${queryParams.length};
+  `;
+
+  // Execute the query and return the results
+  return db.query(queryString, queryParams)
+    .then(res => res.rows)
+    .catch(err => {
+      console.error("Error fetching filtered products:", err.message);
+      throw err;
+    });
 };
 
-module.exports = { filterProducts };
+module.exports = { getFilteredProducts };
