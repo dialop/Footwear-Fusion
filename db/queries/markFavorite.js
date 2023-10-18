f3b2d521d45281d6e0b1b34899294ab0927b7248
dialop <dianalop1998@gmail.com>
2023-10-17 22:33:14 -0400

    Add query for mark as favorite

--- db/queries/markFavorite.js
+++ db/queries/markFavorite.js
@@ -3,28 +3,46 @@
 const { query } = require('express');
 const db = require('../connection');
 
-const markFavorites = async(userId, productId) => {
-  try {
-    const result = await db.query('INSERT INTO favorites(user_id, product_id) VALUES($1, $2) RETURNING *;', [userId, productId]);
-    return result.rows[0];
-  } catch (error) {
-    throw new Error(`Failed to mark product with ID ${productId} as favorite for user with ID ${userId}: ${error.message}`);
-  }
+const markAsFavorite = (productId) => {
+  return db.query(`
+    INSERT INTO user_favorites (user_id, product_id)
+    VALUES ($1, $2)
+    ON CONFLICT (user_id, product_id) DO NOTHING; 
+  `, [productId]);
 };
 
 
 //-- ACCESS USER PRODUCTS THAT HAVE BEEN MARKED AS FAVORITE --//
+
+
 const getFavoritesForUser = async() => {
   const hardcodedUserId = 2;  // Hardcoded user id
+
   try {
-    const result = await db.query('SELECT product_id FROM favorites WHERE user_id = $1;', [hardcodedUserId]);
-    console.log(result.rows, "query", hardcodedUserId);
-    return result.rows;
+      const result = await db.query(`
+      SELECT 
+      p.title,
+      p.model,
+      p.description,
+      p.size,
+      p.color,
+      p.price,
+      p.owner_id,
+      p.photo_url
+  FROM
+      favorites f
+  LEFT JOIN 
+      products p ON f.product_id = p.id 
+  WHERE 
+      f.user_id = $1;
+`, [hardcodedUserId]);
+      console.log(result.rows, "query", hardcodedUserId);
+      return result.rows;
   } catch (error) {
-    throw new Error(`Error retrieving favorites for user with ID ${hardcodedUserId}: ${error.message}`);
+      throw new Error(`Error retrieving favorites for user with ID ${hardcodedUserId}: ${error.message}`);
   }
 };
 
 
 
-module.exports = { markFavorites, getFavoritesForUser };
+module.exports = { markAsFavorite, getFavoritesForUser };
