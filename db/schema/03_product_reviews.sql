55707212a3266b2d4fa02548d2c91655fd2412b4
dialop <dianalop1998@gmail.com>
2023-10-19 20:34:25 -0400

    Add comments, clean code

--- db/schema/03_product_reviews.sql
+++
@@ -1,9 +0,0 @@
--- DROP and CREATE products_reviews TABLE
-
-DROP TABLE IF EXISTS reviews CASCADE;
-CREATE TABLE reviews (
-    id SERIAL PRIMARY KEY,
-    user_id INT REFERENCES users(id),
-    product_id INT REFERENCES products(id),
-    content TEXT NOT NULL
-);
