8af66045edacce7512875c781e2b5a62abfe4c6c
dialop <dianalop1998@gmail.com>
2023-10-19 22:01:23 -0400

    Describe the changes you made

--- db/queries/markFavorite.js
+++ db/queries/markFavorite.js
@@ -11,17 +11,8 @@ const markAsFavorite = (productId) => {
   `, [productId]);
 };
 
-<<<<<<< HEAD
 // Function to retrieve products marked as favorites for a user
-const getFavoritesForUser = async() => {
-  const hardcodedUserId = 2;  // Hardcoded user id
-=======
-
-//-- ACCESS USER PRODUCTS THAT HAVE BEEN MARKED AS FAVORITE --//
-
-
 const getFavoritesForUser = async(userId) => {
->>>>>>> a2edd879383f7cd37b256fa7f66ed7a00bc53e5c
 
   try {
     const result = await db.query(`
@@ -41,18 +32,11 @@ const getFavoritesForUser = async(userId) => {
       products p ON f.product_id = p.id 
   WHERE 
       f.user_id = $1;
-<<<<<<< HEAD
-`, [hardcodedUserId]);
-    return result.rows;
-  } catch (error) {
-    throw new Error(`Error retrieving favorites for user with ID ${hardcodedUserId}: ${error.message}`);
-=======
 `, [userId]);
       console.log('from db', userId);
       return result.rows;
   } catch (error) {
       throw new Error(`Error retrieving favorites for user with ID ${userId}: ${error.message}`);
->>>>>>> a2edd879383f7cd37b256fa7f66ed7a00bc53e5c
   }
 };
 
@@ -60,19 +44,11 @@ const getFavoritesForUser = async(userId) => {
 function deleteFavorite(productId) {
 
   return new Promise((resolve, reject) => {
-<<<<<<< HEAD
-    db.query("DELETE FROM favorites WHERE product_id = ?", [productId], function(err, results) {
-      
-      if (err) return reject(err);
-      resolve(results);
-    });
-=======
       // Example with a hypothetical DB library:
       db.query("DELETE FROM favorites WHERE product_id = $1", [productId], function(err, results) {
           if (err) return reject(err);
           resolve(results);
       });
->>>>>>> a2edd879383f7cd37b256fa7f66ed7a00bc53e5c
   });
 }
 
