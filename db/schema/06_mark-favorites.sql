84cfa5be7d60a001d9ffbf74badda160fa28180e
dialop <dianalop1998@gmail.com>
2023-10-21 00:15:22 -0400

    Add readme file

--- db/schema/06_mark-favorites.sql
+++
@@ -1,9 +0,0 @@
--- SQL Script to Create the 'user_favorites' Table
-
-CREATE TABLE user_favorites (
-  user_id INT,
-  product_id INT,
-  PRIMARY KEY (user_id, product_id),
-  FOREIGN KEY (user_id) REFERENCES users(id),
-  FOREIGN KEY (product_id) REFERENCES products(id)
-);
